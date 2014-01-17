2efd51fcd121931eab6ee5f764147b88342ebe1d
corbin dunn <corbin@corbinstreehouse.com>
2014-01-16 20:50:06 -0800

    Initial Commit

---
+++ CyrWheelPatternEditor/CDDocument.h
@@ -0,0 +1,13 @@
+//
+//  CDDocument.h
+//  CyrWheelPatternEditor
+//
+//  Created by Corbin Dunn on 1/16/14 .
+//  Copyright (c) 2014 Corbin Dunn. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface CDDocument : NSPersistentDocument
+
+@end
---
+++ CyrWheelPatternEditor/CDDocument.m
@@ -0,0 +1,40 @@
+//
+//  CDDocument.m
+//  CyrWheelPatternEditor
+//
+//  Created by Corbin Dunn on 1/16/14 .
+//  Copyright (c) 2014 Corbin Dunn. All rights reserved.
+//
+
+#import "CDDocument.h"
+
+@implementation CDDocument
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Add your subclass-specific initialization here.
+    }
+    return self;
+}
+
+- (NSString *)windowNibName
+{
+    // Override returning the nib file name of the document
+    // If you need to use a subclass of NSWindowController or if your document supports multiple NSWindowControllers, you should remove this method and override -makeWindowControllers instead.
+    return @"CDDocument";
+}
+
+- (void)windowControllerDidLoadNib:(NSWindowController *)aController
+{
+    [super windowControllerDidLoadNib:aController];
+    // Add any code here that needs to be executed once the windowController has loaded the document's window.
+}
+
++ (BOOL)autosavesInPlace
+{
+    return YES;
+}
+
+@end
