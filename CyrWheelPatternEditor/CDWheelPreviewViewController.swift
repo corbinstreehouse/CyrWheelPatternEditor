28ef3f524192d8121152c2e33f4dbf45bc820df5
corbin dunn <corbind@apple.com>
2016-01-05 14:42:30 -1000

    More wip

---
+++ CyrWheelPatternEditor/CDWheelPreviewViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CDWheelPreviewViewController.swift
+//  CyrWheelPatternEditor
+//
+//  Created by corbin dunn on 1/5/16.
+//  Copyright © 2016 Corbin Dunn. All rights reserved.
+//
+
+import Cocoa
+
+class CDWheelPreviewViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
