81ace6a398bbb60e79b4fae11fbd8ea3f7d896cd
corbin dunn <corbin@corbinstreehouse.com>
2016-04-16 09:07:33 -0700

    Fix pool release

--- CDSimulatorLEDPatterns.cpp
+++ CDSimulatorLEDPatterns.cpp
@@ -10,12 +10,12 @@
 
 
 void CDSimulatorLEDPatterns::internalShow() {
-    for (int i = 0; i < getLEDCount(); i++) {
-        NSColor *color = [NSColor colorWithSRGBRed:m_leds[i].r/255.0 green:m_leds[i].g/255.0 blue:m_leds[i].b/255.0 alpha:1.0];
-        [m_cyrWheelView setPixelColor:color atIndex:i];
+    @autoreleasepool {
+        for (int i = 0; i < getLEDCount(); i++) {
+            NSColor *color = [NSColor colorWithSRGBRed:m_leds[i].r/255.0 green:m_leds[i].g/255.0 blue:m_leds[i].b/255.0 alpha:1.0];
+            [m_cyrWheelView setPixelColor:color atIndex:i];
+        }
     }
     [m_cyrWheelView setNeedsDisplay:YES];
-    //#warning corbin force display not so great... slows stuff down considerably..
-    //    [m_cyrWheelView displayIfNeeded];
 }
 
